09ad08cdbc25b450d4cefd1a1b5e2d02cb61b979
neosInPocket <88581498+neosInPocket@users.noreply.github.com>
2023-10-24 20:11:23 +0300

    changes

--- Assets/Scripts/MainMenu/MenuItemsController.cs
+++ Assets/Scripts/MainMenu/MenuItemsController.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuItemsController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GoToPlayScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    	SceneManager.LoadScene("MainGameScene");
     }
 }
--- Assets/Scripts/MainMenu/ShopController.cs
+++ Assets/Scripts/MainMenu/ShopController.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Image[] speedUpgradesStars;
+	[SerializeField] private Image[] lifesUpgradesStars;
+	[SerializeField] private Button SpeedButton;
+	[SerializeField] private Button lifesButton;
+	[SerializeField] private TMPro.TMP_Text coinsAmount;
+	
+	public void Refresh()
+	{
+		PlayerSaveLoad.Load();
+		RefreshPoints();
+		coinsAmount.text = PlayerSaveLoad.CurrentCoins.ToString();
+		
+		if (PlayerSaveLoad.CurrentCoins < 50 || PlayerSaveLoad.SpeedUpgrade == 3)
+		{
+			SpeedButton.interactable = false;
+		}
+		
+		if (PlayerSaveLoad.CurrentCoins < 100 || PlayerSaveLoad.CurrentLifesUpgrade == 3)
+		{
+			lifesButton.interactable = false;
+		}
+	}
+	
+	public void LifesAmountUpgrade()
+	{
+		PlayerSaveLoad.CurrentCoins -= 100;
+		PlayerSaveLoad.CurrentLifesUpgrade++;
+		PlayerSaveLoad.Save();
+		Refresh();
+	}
+	
+	public void SpeedUpgrade()
+	{
+		PlayerSaveLoad.CurrentCoins -= 50;
+		PlayerSaveLoad.SpeedUpgrade++;
+		PlayerSaveLoad.Save();
+		Refresh();
+	}
+	
+	private void RefreshPoints()
+	{
+		foreach (var gravity in speedUpgradesStars)
+		{
+			gravity.color = new Color(1, 1, 1, 0);
+		}
+		
+		for (int i = 0; i < PlayerSaveLoad.SpeedUpgrade; i++)
+		{
+			speedUpgradesStars[i].color = new Color(1, 1, 1, 1);
+		}
+		
+		foreach (var life in lifesUpgradesStars)
+		{
+			life.color = new Color(1, 1, 1, 0);
+		}
+		
+		for (int i = 0; i < PlayerSaveLoad.CurrentLifesUpgrade; i++)
+		{
+			lifesUpgradesStars[i].color = new Color(1, 1, 1, 1);
+		}
+	}
 }
